8fc1d8ec49d314e08f42cf7cf21db10257eb2881
hydroperx <hydroperfox@gmail.com>
2025-09-18 11:18:46 -0300

    upd

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is an alternative to using complex template engines.
 ## Example
 
 ```rust
-use fairyvoid_lateformat::LateFormat;
+use hydroperx_lateformat::LateFormat;
 use maplit::hashmap;
 
 let user_string: String = "some user string: {id}".into();
--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,7 @@ use lazy_regex::*;
 /// # Example
 /// 
 /// ```
-/// use fairyvoid_lateformat::LateFormat;
+/// use hydroperx_lateformat::LateFormat;
 /// use maplit::hashmap;
 /// let user_string: String = "some user string: {id}".into();
 /// assert_eq!("some user string: x", user_string.lateformat(hashmap!{"id".into() => "x".into()}));
